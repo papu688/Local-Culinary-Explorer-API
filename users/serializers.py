24d4e5692325f1393c5ba8da963cf1bdc5de7473
papu_68 <giorgobianipaul@gmail.com>
2024-08-31 16:58:18 +0200

    Add serializers for new models

---
+++ users/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import User, Chef, Ingredient, Dish, Rating
+
+class Userserializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio']
+
+class ChefSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chef
+        fields = ['id', 'user', 'name', 'bio', 'specialty']
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name', 'description']
+
+class DishSerializer(serializers.ModelSerializer):
+    model = Dish
+    fields = ['id', 'name', 'description', 'cooking_method', 'photo', 'chef', 'ingredients']
+
+class RatingSerializer(serializers.Modelserializer):
+    class Meta:
+        model = Rating
+        fields = ['id', 'user', 'dish', 'rating']
